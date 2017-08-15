665ae1a2b3157f676c2973795ef418b2288078d8
Deepak <deepak.sorthiya@gmail.com>
2017-08-15 16:26:41 +0530

    Return type added.

--- src/main/java/com/example/SpringBootJspApplication.java
+++ src/main/java/com/example/SpringBootJspApplication.java
@@ -2,11 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootJspApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJspApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootJspApplication.class, args);
 	}
 }
